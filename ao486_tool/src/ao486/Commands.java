26e15f11dc0caee6ea19413e8dfade7a56e6f1a8
Zeus Gómez Marmolejo <zeus@aluzina.org>
2016-01-26 16:21:32 +0100

    Corrected some relative paths in java programs.

--- ao486_tool/src/ao486/Commands.java
+++ ao486_tool/src/ao486/Commands.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2014, Aleksander Osman
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,77 +32,77 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Commands {
-    
+
     static void parse(String name, Parse parse) throws Exception  {
         for(String key : commands.keySet()) {
             String contents = commands.get(key);
-            
+
             String start = "<"  + name + ">";
             String end   = "</" + name + ">";
-            
+
             while(true) {
                 int start_index = contents.indexOf(start);
                 int end_index   = contents.indexOf(end);
-                
+
                 if((start_index != -1 && end_index == -1) || (start_index == -1 && end_index != -1)) throw new Exception("Invalid <" + name + "> section in file: " + key);
-                
+
                 if(start_index != -1 && end_index != -1) {
                     String section = contents.substring(start_index+start.length(), end_index);
-                    
+
                     parse.parse(section.trim());
-                    
+
                     contents = contents.replaceFirst("(?s)<" + name + ">.*?</" + name + ">", "");
                     commands.put(key, contents);
                 }
-                
+
                 if(start_index == -1 && end_index == -1) break;
             }
         }
     }
-    
+
     static void read_command_files(File command_directory) throws Exception {
         if(command_directory.exists() == false || command_directory.isDirectory() == false) throw new Exception("Can not find commands directory: " + command_directory.getCanonicalPath());
-        
+
         commands = new HashMap<>();
-        
+
         //read all files
         for(File file : command_directory.listFiles()) {
             commands.put(file.getName(), new String(Files.readAllBytes(file.toPath())));
         }
-        
+
         //remove all comments and empty lines
         for(String key : commands.keySet()) {
             String contents = commands.get(key);
-            
+
             contents = contents.replaceAll("//[^\\n]*", "");
             contents = contents.replaceAll("\\n\\s*\\n", "\n");
             contents = contents.trim();
             contents = contents.replaceAll("\\n\\s+", " ");
-            
+
             //System.out.println(contents);
             commands.put(key, contents);
         }
-        
+
         //parse defines
         HashSet<Integer> command_values = new HashSet<>();
         command_values.add(0); //can not use zero
-        
+
         defines = new HashMap<>();
         parse("defines", new ParseDefine(defines, command_values));
-        
+
         //parse decode
         decode = new StringBuilder();
         parse("decode", new ParseDecode(decode));
-        
+
         //parse microcode
         microcode = new StringBuilder();
         StringBuilder microcode_loop = new StringBuilder();
-        
-        
+
+
         parse("microcode", new ParseMicrocode(defines, microcode, microcode_loop));
-        
+
         microcode_loop.append("\n");
-        
+
         microcode.append("IF(\n");
         microcode.append(microcode_loop);
         microcode.append(");\n");
@@ -110,60 +110,60 @@ public class Commands {
         microcode.append("\tSET(mc_cmdex_current, mc_cmdex_last);\n");
         microcode.append("\tSET(mc_cmd_next,      mc_cmd);\n");
         microcode.append("ENDIF();\n\n");
-        
+
         //parse read_local
         read_local = new StringBuilder();
         parse("read_local", new ParseToString(read_local));
-        
+
         //parse read
         read = new StringBuilder();
         parse("read", new ParseToString(read));
-        
+
         //parse execute_local
         execute_local = new StringBuilder();
         parse("execute_local", new ParseToString(execute_local));
-        
+
         //parse execute
         execute = new StringBuilder();
         parse("execute", new ParseToString(execute));
-        
+
         //parse write_local
         write_local = new StringBuilder();
         parse("write_local", new ParseToString(write_local));
-        
+
         //parse write
         write = new StringBuilder();
         parse("write", new ParseToString(write));
-        
+
         //check if all commands are empty
         for(String name : commands.keySet()) {
             String left = commands.get(name);
             left = left.trim();
-            
+
             if(left.equals("") == false) {
                 throw new Exception("Unrecognized file part: " + name + "\n" + left);
             }
         }
     }
-    
+
     public static void main(String args[]) throws Exception {
-        read_command_files(new File("./../rtl/commands"));
+        read_command_files(new File("./../rtl/ao486/commands"));
     }
-    
+
     static HashMap<String, String> defines;
-    
+
     static StringBuilder decode;
-    
+
     static StringBuilder microcode;
-    
+
     static StringBuilder read_local;
     static StringBuilder read;
-    
+
     static StringBuilder execute_local;
     static StringBuilder execute;
-    
+
     static StringBuilder write_local;
     static StringBuilder write;
-    
+
     static HashMap<String, String> commands;
 }
--- ao486_tool/src/ao486/test/VerilogFreeRun.java
+++ ao486_tool/src/ao486/test/VerilogFreeRun.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (c) 2014, Aleksander Osman
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -32,14 +32,14 @@ import java.util.LinkedList;
 import java.util.Random;
 
 public class VerilogFreeRun extends TestUnit {
-    
+
     @Override public int get_test_count() throws Exception { throw new UnsupportedOperationException("Not supported yet."); }
 
-    @Override public void init() throws Exception { 
+    @Override public void init() throws Exception {
         random = new Random(10);
-        
+
         layers.clear();
-        
+
         final Random random_final = random;
         layers.addFirst(new Layer() {
             Byte get_memory(long address) {
@@ -48,7 +48,7 @@ public class VerilogFreeRun extends TestUnit {
                 if(address == 0xFFFFFFF2L) return (byte)0xFF;
                 if(address == 0xFFFFFFF3L) return (byte)0x00;
                 if(address == 0xFFFFFFF4L) return (byte)0xF0;
-                
+
                 /*
                 if(address == 0x000FFF00L) return (byte)0x66;
                 if(address == 0x000FFF01L) return (byte)0xB8;
@@ -71,7 +71,7 @@ public class VerilogFreeRun extends TestUnit {
                 if(address == 0x000FFF12L) return (byte)0xEB;
                 if(address == 0x000FFF13L) return (byte)0xFE;
                 */
-                
+
                 /*
                 if(address == 0x000FFF00L) return (byte)0x89;
                 if(address == 0x000FFF01L) return (byte)0x87;
@@ -90,15 +90,15 @@ public class VerilogFreeRun extends TestUnit {
                 if(address == 0x000FFF0EL) return (byte)0x1E;
                 if(address == 0x000FFF0FL) return (byte)0x00;
                 */
-                
-                
+
+
                 if(address == 0x000FFF00L) return (byte)0xEA;
                 if(address == 0x000FFF01L) return (byte)0x00;
                 if(address == 0x000FFF02L) return (byte)0x00;
                 if(address == 0x000FFF03L) return (byte)0x70;
                 if(address == 0x000FFF04L) return (byte)0x00;
-                
-                
+
+
                 if(address == 0x00000700L) return (byte)0x2E;
                 if(address == 0x00000701L) return (byte)0x89;
                 if(address == 0x00000702L) return (byte)0x1E;
@@ -115,30 +115,30 @@ public class VerilogFreeRun extends TestUnit {
                 if(address == 0x0000070DL) return (byte)0x2D;
                 if(address == 0x0000070EL) return (byte)0x01;
                 if(address == 0x0000070FL) return (byte)0x00;
-                
+
                 return (byte)(random_final.nextInt() & 0xFFL);
             }
         });
     }
-    
+
     public static void main(String args[]) throws Exception {
-        
+
         Class test_class = VerilogFreeRun.class;
-        
+
         File output_directory = new File(".");
         File directory;
         LinkedList<String> command_line = new LinkedList<>();
-        
+
         Runner runner = new Runner();
-        
+
         //run verilog
         TestUnit test_verilog = (TestUnit)test_class.newInstance();
 
         test_verilog.init();
 
-        directory = new File("./../sim/ao486_run");
+        directory = new File("./../sim/iverilog/ao486_run");
         command_line.clear();
-        command_line.add("/opt/iverilog/bin/vvp");
+        command_line.add("/usr/bin/vvp");
         command_line.add("tb_ao486_run.vvp");
         //command_line.add("-lxt");
 
